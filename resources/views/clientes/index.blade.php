15c2a96c075e4c5ef1200e35ddac5c415b4ecd3a
oscarfiscal <oscarfiscalcp@gmail.com>
2022-04-05 15:48:09 -0500

    vista para crear cliente

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -9,7 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <a type="button" href="{{ route('clientes.create') }}"  style="float: right;" class=" bg-indigo-600 px-12 py-2 rounded text-gray-200 font-semibold hover:bg-indigo-800 transition duration-200 each-in-out">Crear</a>
-            <table class="table-fixed w-full">
+           
+                <table class="table-auto w-full">
                 <thead>
                     <tr class="bg-blue-700 text-white">
                         <th style="display: none;">Id</th>
@@ -37,10 +38,10 @@
                         <td class="border px-4 py-2">
                             <div class="flex justify-center rounded-lg text-lg" role="group">
                                 <!-- botón editar -->
-                                <a  class="rounded bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4">Editar</a>
+                                <a href="{{ route('clientes.edit', $client->id) }}" class="rounded bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4">Editar</a>
 
                                 <!-- botón borrar -->
-                                <form action="" method="POST" class="formEliminar">
+                                <form action="{{ route('clientes.destroy', $client->id) }}" method="POST" class="formEliminar">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit"  style="background-color:red;" class="rounded text-white font-bold py-2 px-4">Borrar</button>
