40f9031f4bb027415a3c18eff8642635835baefb
oscarfiscal <oscarfiscalcp@gmail.com>
2022-04-05 18:48:34 -0500

    actualizar cliente

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -49,9 +49,9 @@ class ClientController extends Controller
 
         if($imagen = $request->file('imagen')) {
             $rutaGuardarImg = 'imagen/';
-            $imagenProducto = date('YmdHis'). "." . $imagen->getClientOriginalExtension();
-            $imagen->move($rutaGuardarImg, $imagenProducto);
-            $client['imagen'] = "$imagenProducto";             
+            $imagenClient = date('YmdHis'). "." . $imagen->getClientOriginalExtension();
+            $imagen->move($rutaGuardarImg, $imagenClient);
+            $client['imagen'] = "$imagenClient";             
         }
         
         Client::create($client);
@@ -75,9 +75,11 @@ class ClientController extends Controller
      * @param  \App\Models\Client  $client
      * @return \Illuminate\Http\Response
      */
-    public function edit(Client $client)
+    public function edit(Client $cliente)
     {
-        //
+        return view('clientes.editar', compact('cliente'));
+       
+    
     }
 
     /**
@@ -87,10 +89,20 @@ class ClientController extends Controller
      * @param  \App\Models\Client  $client
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Client $client)
+    public function update(Request $request, Client $cliente)
     {
-        //
-    }
+        $client = $request->all();  //obtiene todos los datos del formulario
+        if($imagen = $request->file('imagen')){
+           $rutaGuardarImg = 'imagen/';
+           $imagenClient = date('YmdHis') . "." . $imagen->getClientOriginalExtension(); 
+           $imagen->move($rutaGuardarImg, $imagenClient);
+           $client['imagen'] = "$imagenClient";
+        }else{
+           unset($client['imagen']);
+        }   //obtiene la imagen y la guarda en la ruta especificada
+        $cliente->update($client); //actualiza los datos del cliente
+        return redirect()->route('clientes.index'); //redirecciona a la ruta clientes
+   }
 
     /**
      * Remove the specified resource from storage.
