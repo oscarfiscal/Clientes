78cacd5e4a54372aa64a3b266631363eab0007e4
oscarfiscal <oscarfiscalcp@gmail.com>
2022-04-05 12:16:52 -0500

    test para listar clientes

--- tests/Feature/Http/Controllers/ClientTest.php
+++ tests/Feature/Http/Controllers/ClientTest.php
@@ -14,13 +14,26 @@ class ClientTest extends TestCase
      *
      * @return void
      */
+    //test  para listar clientes
+    
+    public function test_list_clients()
+
+    {
+        $this->withoutExceptionHandling();
+
+       Client::factory()->create(); // crea un cliente
+                   
+        $response=$this->get('clientes')->assertStatus(200); //obtiene la ruta clientes y verifica que el status sea 200
+        $producto=Client::all();  //obtiene todos los clientes 
+        $response->assertOk();  //cuando obtenga los clientes verifica que el estado sea 200
+    }
    
      //test crear un cliente 
 
     public function test_create_client()
     {
-        $this->withoutExceptionHandling();
-        
+        $this->withoutExceptionHandling(); 
+
         $response = $this->post('/clients', [
             'nombre' => 'oscar fiscal',
             'imagen' => 'url.jpg',
@@ -28,7 +41,7 @@ class ClientTest extends TestCase
              'correo' => 'oscar@example.com',
              'telefono' => '213112331',
              'observaciones' => 'desarrollador',
-        ])->assertRedirect('clientes');
+        ])->assertRedirect('clientes');     // cuando se envien los datos redirecciona a la ruta clientes
             
         $this->assertDatabaseHas('clients', [
             'nombre' => 'oscar fiscal',
@@ -37,7 +50,7 @@ class ClientTest extends TestCase
             'correo' => 'oscar@example.com',
             'telefono' => '213112331',
             'observaciones'=>'desarrollador',
-        ]);     
+        ]);    // verifica que el cliente se haya creado en la base de datos  
     
     }
 }
